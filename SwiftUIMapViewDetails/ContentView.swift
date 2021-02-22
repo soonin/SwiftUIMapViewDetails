f6ac5e52e377da2a29dc45e4eb195f9621a91e7d
pooya <info@centurytrail.com>
2021-02-22 03:16:50 -0800

    check comments for ios 14

--- SwiftUIMapViewDetails/ContentView.swift
+++ SwiftUIMapViewDetails/ContentView.swift
@@ -8,6 +8,8 @@
 
 import SwiftUI
 
+
+//iOS <= 13.4
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -34,3 +36,26 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+//iOS > 13.4 and 14
+/*
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+*/
+
