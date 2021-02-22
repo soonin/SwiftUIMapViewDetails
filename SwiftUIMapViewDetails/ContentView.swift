6b5cd82fe4104a91a08dae23cfe899c94d9321f6
pooya <info@centurytrail.com>
2021-02-22 02:47:19 -0800

    Initial Commit

---
+++ SwiftUIMapViewDetails/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMapViewDetails
+//
+//  Created by Pooya on 2021-02-22.
+//  Copyright © 2021 Pooya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
